c235866e8266a1ca2b59c08b5d0af2d066412798
Tim Diekmann <td@hash.ai>
2023-06-15 13:36:46 +0200

    TMP

--- apps/hash-ai-worker-ts/src/activities.ts
+++ apps/hash-ai-worker-ts/src/activities.ts
@@ -94,41 +94,41 @@ export const createGraphActivities = (createInfo: {
     entityTypeId: VersionedUrl;
     graphResolveDepths?: Partial<GraphResolveDepths>;
   }): Promise<any> {
-    const subgraph = await getEntityTypeSubgraphById(createInfo.graphContext, {
-      entityTypeId: params.entityTypeId,
-      graphResolveDepths: {
-        ...zeroedGraphResolveDepths,
-        ...params.graphResolveDepths,
-        constrainsPropertiesOn: { outgoing: 1 },
-      },
-      temporalAxes: currentTimeInstantTemporalAxes,
-      actorId: createInfo.actorId,
-    });
+    // const subgraph = await getEntityTypeSubgraphById(createInfo.graphContext, {
+    //   entityTypeId: params.entityTypeId,
+    //   graphResolveDepths: {
+    //     ...zeroedGraphResolveDepths,
+    //     ...params.graphResolveDepths,
+    //     constrainsPropertiesOn: { outgoing: 1 },
+    //   },
+    //   temporalAxes: currentTimeInstantTemporalAxes,
+    //   actorId: createInfo.actorId,
+    // });
 
-    const roots = getRoots(subgraph);
-    if (roots.length !== 1) {
-      throw new Error(
-        `Expected exactly one root entity, but found ${roots.length} roots.`,
-      );
-    }
-    const root = roots[0]!;
+    // const roots = getRoots(subgraph);
+    // if (roots.length !== 1) {
+    //   throw new Error(
+    //     `Expected exactly one root entity, but found ${roots.length} roots.`,
+    //   );
+    // }
+    // const root = roots[0]!;
 
-    const title = root.schema.title;
-    const description = root.schema.description;
+    // const title = root.schema.title;
+    // const description = root.schema.description;
 
-    const rootEdges =
-      subgraph.edges[root.metadata.recordId.baseUrl]?.[
-        root.metadata.recordId.version.toString() as OntologyTypeRevisionId
-      ];
+    // const rootEdges =
+    //   subgraph.edges[root.metadata.recordId.baseUrl]?.[
+    //     root.metadata.recordId.version.toString() as OntologyTypeRevisionId
+    //   ];
 
-    const rootPropertyTypes = rootEdges
-      ?.filter(({ kind }) => kind === "CONSTRAINS_PROPERTIES_ON")
-      .map(({ rightEndpoint }) => {
-        return getPropertyTypeByVertexId(
-          subgraph,
-          rightEndpoint as OntologyTypeVertexId,
-        )!;
-      });
+    // const rootPropertyTypes = rootEdges
+    //   ?.filter(({ kind }) => kind === "CONSTRAINS_PROPERTIES_ON")
+    //   .map(({ rightEndpoint }) => {
+    //     return getPropertyTypeByVertexId(
+    //       subgraph,
+    //       rightEndpoint as OntologyTypeVertexId,
+    //     )!;
+    //   });
 
     const apiKey = getRequiredEnv("OPENAI_API_KEY");
 
@@ -137,84 +137,89 @@ export const createGraphActivities = (createInfo: {
     });
     const openai = new OpenAIApi(configuration);
 
-    const prompt = `
-    Character 1: Amelia Hartley
-    Address: 452 Willow Lane, Evergreen Heights
-    
-    Amelia Hartley, a vivacious young artist with a passion for vibrant colors, resides at 452 Willow Lane in the charming neighborhood of Evergreen Heights. Her cozy address is adorned with blooming flowers and adorned windows, reflecting her imaginative spirit. Within the walls of her quaint cottage, Amelia creates breathtaking paintings that transport viewers to dreamlike realms.
-    
-    Character 2: Jackson Bennett
-    Address: 725 Oakwood Avenue, Ravenwood Manor
-    
-    At 725 Oakwood Avenue, nestled within the enigmatic Ravenwood Manor, resides Jackson Bennett—a brooding writer known for his mesmerizing tales of mystery and suspense. Shadows dance across the imposing manor's ivy-clad facade, hinting at the secrets concealed within. Jackson's study overlooks the sprawling gardens, providing him with inspiration as he weaves intricate plots that captivate readers worldwide.
-    
-    Character 3: Lily Chen
-    Address: 317 Cherry Blossom Lane, Serenity Meadows
-    
-    In the idyllic neighborhood of Serenity Meadows, a tranquil abode awaits at 317 Cherry Blossom Lane, the residence of Lily Chen. Her home is a serene sanctuary, surrounded by fragrant cherry blossom trees that paint the landscape with delicate hues. Lily, a dedicated yoga instructor, opens her doors to students seeking balance and mindfulness. The gentle ambiance of her address serves as a testament to her calming presence.
-    
-    Character 4: Max Cooper
-    Address: 912 Harborview Terrace, Ocean's Edge
-    
-    Perched on the edge of a picturesque seaside cliff, Max Cooper's address at 912 Harborview Terrace provides a breathtaking view of the vast ocean expanse. His modern beachfront retreat in Ocean's Edge is a testament to his adventurous spirit and love for the sea. Max, an avid marine biologist, spends his days exploring the depths, unraveling the mysteries of the ocean's inhabitants, and returning home to his address that resonates with the soothing sound of crashing waves.
-    `;
+    const prompt = `Amelia Hartley, a vivacious young artist with a passion for vibrant colors, resides at 452 Willow Lane in the charming neighborhood of Evergreen Heights. Her cozy address is adorned with blooming flowers and adorned windows, reflecting her imaginative spirit. Within the walls of her quaint cottage, Amelia creates breathtaking paintings that transport viewers to dreamlike realms.`;
+
+    const schema = {
+      $schema: "http://json-schema.org/draft-07/schema#",
+      title: "Address",
+      type: "object",
+      $comment:
+        "Information required to identify a specific location on the planet associated with a postal address.",
+      properties: {
+        street_address_line_1: {
+          $comment:
+            "The first line of street information of an address. \n\nConforms to the “address-line1” field of the “WHATWG Autocomplete Specification”.\n\nSee: https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#attr-fe-autocomplete-address-level1",
+          oneOf: [
+            {
+              type: "string",
+            },
+          ],
+        },
+        address_level_1: {
+          $comment:
+            "The broadest administrative level in the address, i.e. the province within which the locality is found; for example, in the US, this would be the state; in Switzerland it would be the canton; in the UK, the post town.\n\nCorresponds to the “address-level1” field of the “WHATWG Autocomplete Specification”.\n\nSee: https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#attr-fe-autocomplete-address-level1",
+          oneOf: [
+            {
+              type: "string",
+            },
+          ],
+        },
+        postal_code: {
+          $comment:
+            "The postal code of an address.\n\nThis should conform to the standards of the area the code is from, for example\n\n- a UK postcode might look like: “SW1A 1AA”\n\n- a US ZIP code might look like: “20500”",
+          oneOf: [
+            {
+              type: "string",
+            },
+          ],
+        },
+        alpha_2_country_code: {
+          $comment:
+            "The short-form of a country’s name.\n\nConforms to the ISO 3166 alpha-2 country code specification.\n\nSee: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2",
+          oneOf: [
+            {
+              type: "string",
+            },
+          ],
+        },
+      },
+    };
 
     const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-0613",
       temperature: 0,
       max_tokens: 1500,
+      functions: [
+        {
+          name: "create_entity_type",
+          description: "Creates an entity type from the provided parameters",
+          parameters: schema,
+        },
+      ],
+      function_call: "none",
       messages: [
         {
           role: "system",
-          content: `You are a helper AI to generate entities given a specified entity type. "user" will you provide a text and you
-            shall fill out the fields of the entities mentioned in the text. Don't use other sources to provide
-            information than the provided text "user" will give you. If the text does not match the description use denote it as "null".`,
+          content: `You are a LLM and extract information from an unstructured text. The user will you provide this text and 
+            shall generate the appropriated parameters for the function call. Only use the information which were provided.`,
         },
+        // {
+        //   role: "user",
+        //   content: `Homer Simpson is a fictional character from the animated television series "The Simpsons," and his address within the show is 742 Evergreen Terrace, Springfield. However, it's important to note that "The Simpsons" is a work of fiction, and Springfield is a fictional town, so the address does not correspond to a real location.`,
+        // },
+        // {
+        //   role: "assistant",
+        //   content: `[
+        //     {
+        //       "https://blockprotocol.org/@blockprotocol/types/property-type/street-address-line-1/": "742 Evergreen Terrace",
+        //       "https://blockprotocol.org/@blockprotocol/types/property-type/address-level-1/": "Springfield",
+        //       "https://blockprotocol.org/@blockprotocol/types/property-type/mapbox-full-address/": "742 Evergreen Terrace, Springfield"
+        //     }
+        //   ]`,
+        // },
         {
           role: "user",
-          content: `
-            Title: Person
-            Description: Represents an individual human being. It encapsulates the attributes and characteristics that define a person's identity, including their name, age, and gender.
-            Properties: 
-             - Title: Name
-               Id: https://blockprotocol.org/@blockprotocol/types/property-type/name/v/1
-               Description: The full name of a person.
-             - Title: Age
-               Id: https://blockprotocol.org/@blockprotocol/types/property-type/age/v/4
-               Description: The age of a person.
-             - Title: Gender
-               Id: https://hash.org/@alice/types/property-type/gender/v/2
-               Description: The gender of a person.
-            Text: Albert Einstein; 14 March 1879 – 18 April 1955) was a German-born theoretical physicist. Best known for developing the theory of relativity, he also made important contributions to the development of the theory of quantum mechanics, and thus to modern physics. His mass–energy equivalence formula E = mc2, which arises from relativity theory, has been dubbed "the world's most famous equation". His work is also known for its influence on the philosophy of science. He received the 1921 Nobel Prize in Physics "for his services to theoretical physics, and especially for his discovery of the law of the photoelectric effect", a pivotal step in the development of quantum theory.Einsteinium, one of the synthetic elements in the periodic table, was named in his honor.
-          `,
-        },
-        {
-          role: "assistant",
-          content: `[
-            {
-              "https://blockprotocol.org/@blockprotocol/types/property-type/name/" : "Albert Einstein",
-              "https://blockprotocol.org/@blockprotocol/types/property-type/age/" : 76,
-              "https://hash.org/@alice/types/property-type/gender/": "male"
-            }
-          ]`,
-        },
-        {
-          role: "user",
-          content: `
-            Title: ${title}
-            Description: ${description!}
-            Properties: 
-            ${rootPropertyTypes!
-              .map(
-                (propertyType) => `
-              - Title: ${propertyType.schema.title}
-                Id: ${propertyType.schema.$id}
-                Description: ${propertyType.schema.description!}
-            `,
-              )
-              .join("\n")}
-            Text: ${prompt}
-          `,
+          content: prompt,
         },
       ],
     });
